fc74a3e22e9ec516a23783f1bb69f5af7ee4da61
Đoàn VIệt Anh <hongnhungdoan161@gmail.com>
2018-11-28 20:24:51 +0700

    .

---
+++ data.js
@@ -0,0 +1,18 @@
+var data = [
+    {
+        id: "1",
+        name: 'TenBaiHat1',
+        url: '',
+        thumbnail: '',
+        image: '',
+
+    },
+    {
+        id: '2',
+        name: '',
+        url: '',
+        thumbnail: '',
+        image: '',
+    },
+    
+]
