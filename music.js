56f836a295d9a303c3a434f7697598c996ddc9d1
donezombie <donezombie@gmail.com>
2018-11-29 15:25:19 +0700

    .

--- music.js
+++ music.js
@@ -1,7 +1,12 @@
-let dataGet;
-var a = $.getJSON('data.json')
-console.log(a);
-console.log(a.responseJSON());
+const run = async () => {
+    const dataAfterGet = await getDataFromJson('data.json');
+    // Write Code JS here
+    console.log(dataAfterGet);
+    dataAfterGet.forEach(el => console.log(el))
+    
+
+    //____________________
+}
 
 
-
+run();  
