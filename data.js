5a85fa2d5c241242d3cc4d9646964dd593e8fae5
Đoàn VIệt Anh <hongnhungdoan161@gmail.com>
2018-12-04 00:09:41 +0700

    .

---
+++ data.js
@@ -0,0 +1,23 @@
+var dataMusic = [
+    {
+        id: 1,
+        name: 'thang dien',
+        thumbnail: '....',
+        url: 'https://images.pexels.com/photos/1649649/pexels-photo-1649649.jpeg?auto=compress&cs=tinysrgb&h=350',
+        author: '....'
+    },
+    {
+        id: 2,
+        name: 'thang dien',
+        thumbnail: '....',
+        url: 'https://images.pexels.com/photos/1649649/pexels-photo-1649649.jpeg?auto=compress&cs=tinysrgb&h=350',
+        author: '....'
+    },
+    {
+        id: 3,
+        name: 'thang dien',
+        thumbnail: '....',
+        url: 'https://images.pexels.com/photos/1649649/pexels-photo-1649649.jpeg?auto=compress&cs=tinysrgb&h=350',
+        author: '....'
+    },
+]
--- dataHandle.js
+++ dataHandle.js
@@ -2,3 +2,20 @@ const getDataFromJson = (url) => {
     const data = $.getJSON(url);
     return data
 }
+
+//dataMusic la 1 list gom nhieu phan tu
+
+//forEach la duyet tung phan tu 1 trong list
+var DOMlistmusic = document.getElementById('listMusic');
+dataMusic.forEach(function(music){
+    var HTML = `
+        <div>
+            <p>${music.id}</p>
+            <p>${music.name}</p>
+            <p>${music.thumbnail}</p>
+            <img src=${music.url} /> 
+        </div>
+    `;
+
+    DOMlistmusic.insertAdjacentHTML('afterbegin', HTML);
+});
