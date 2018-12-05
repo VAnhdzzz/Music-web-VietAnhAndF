6803404eb217287062167a29e45d2c06aed8d7a7
Đoàn VIệt Anh <hongnhungdoan161@gmail.com>
2018-12-06 01:25:57 +0700

    .

--- mv.js
+++ mv.js
@@ -5,13 +5,25 @@ search = 'thebao';
 fetchData(url_search, 'search', parts_search);
 
 const run = (dataAfterGet) => {
-    console.log(dataAfterGet);
-        // var Indie = document.getElementsByClassName("indie");
-        // Indie.forEach(function(indie){
-        //     for (var i=0;i<)
-        //     var HTML = `
-        //     <iframe src="https://www.youtube.com/watch?v=${id}" frameborder="0"></iframe>
-        //     `
-        // });
+    console.log('cai nay la',dataAfterGet);
+        var Indie = document.getElementsByClassName("indie");
+        Indie.textContent = ''; 
+        for (var i=0;i<dataAfterGet.items.length;i++){
+        var id = dataAfterGet.items[i].id.videoId;
+        console.log (id);
+        var iframe = `
+
+            <iframe src="https://www.youtube.com/watch?v=${id}" frameborder="0"></iframe>
+        `
+        var HTML = `
+        <div class="big-mv">big-mv</div>
+                <div class="column">
+
+                    <div class="small-mv indie">${iframe}</div>
+                    <div class="small-mv indie">${iframe}</div>
+                    <div class="small-mv none-bot indie">${iframe}</div>    
+            `
+        // Indie.insertAdjacentHTML("beforeend",HTML);
+        }
 
     }
