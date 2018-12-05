81db13d13f504314141d609443410347dc30d4e6
vuquocm <vuquocminh2002@gmail.com>
2018-12-05 19:32:02 +0700

    .

---
+++ web-session4-js/event.js
@@ -0,0 +1,6 @@
+function bodyloaded() {
+    console.log("body loaded")
+}
+function btnMouseOver() {
+    console.log("mouseover");
+}
---
+++ web-session4-js/read.js
@@ -0,0 +1,10 @@
+
+function search() {
+    var searchBar = document.getElementById("search_bar");
+    console.log(searchBar.value);
+    console.log(searchBar)
+    console.log(searchBar.getAttribute("data"))
+}
+var btn = document.getElementById("btn")
+console.log(btn.textContent)
+ 
