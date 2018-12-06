52c97da39fd5c19a8157be25af28ea0f9a4e0af0
Đoàn VIệt Anh <hongnhungdoan161@gmail.com>
2018-12-06 13:24:15 +0700

    .

--- mv.js
+++ mv.js
@@ -8,6 +8,7 @@ const run = (dataAfterGet) => {
     console.log('cai nay la',dataAfterGet);
         var Indie = document.getElementsByClassName("indie");
         Indie.textContent = ''; 
+        console.log(Indie)
         for (var i=0;i<dataAfterGet.items.length;i++){
         var id = dataAfterGet.items[i].id.videoId;
         console.log (id);
@@ -16,14 +17,23 @@ const run = (dataAfterGet) => {
             <iframe src="https://www.youtube.com/watch?v=${id}" frameborder="0"></iframe>
         `
         var HTML = `
+        
+        <div class="child">
+        <div class="content-title" >Rap</div>
+        <div class="mv-container">
         <div class="big-mv">big-mv</div>
-                <div class="column">
+        <div class="column">
 
-                    <div class="small-mv indie">${iframe}</div>
-                    <div class="small-mv indie">${iframe}</div>
-                    <div class="small-mv none-bot indie">${iframe}</div>    
+            <div class="small-mv indie">${iframe}</div>
+            <div class="small-mv indie">${iframe}</div>
+            <div class="small-mv none-bot indie">${iframe}</div> 
+            
+        </div>
+    </div>
             `
-        // Indie.insertAdjacentHTML("beforeend",HTML);
+
+            console.log(HTML);
+            document.getElementById("wrap").insertAdjacentHTML("afterbegin",HTML);
         }
 
     }
